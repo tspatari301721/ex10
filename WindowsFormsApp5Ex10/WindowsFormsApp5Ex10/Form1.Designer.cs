16491da62af868a7b26ee7fbba0a4ebf983d50f5
tspatari301721 <84002536+tspatari301721@users.noreply.github.com>
2021-05-15 08:56:00 +0300

    Add files via upload

---
+++ WindowsFormsApp5Ex10/WindowsFormsApp5Ex10/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace WindowsFormsApp5Ex10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(89, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "A";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(160, 270);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 64);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "B";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(231, 270);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 64);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "C";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(370, 309);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(301, 56);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Verifica";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.MouseEnter += new System.EventHandler(this.button4_MouseEnter);
+            this.button4.MouseLeave += new System.EventHandler(this.button4_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(231, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Afiseaza starea butoanelor";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(455, 217);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(81, 34);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(722, 428);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApp5Ex10/WindowsFormsApp5Ex10/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5Ex10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.BackColor == Color.Red) button1.BackColor = Color.Yellow;
+            else button1.BackColor = Color.Red;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.BackColor == Color.Red) button2.BackColor = Color.Yellow;
+            else button2.BackColor = Color.Red;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.BackColor == Color.Red) button3.BackColor = Color.Yellow;
+            else button3.BackColor = Color.Red;
+        }
+        private void button4_MouseEnter(object sender, System.EventArgs e)
+        { button4.BackColor = Color.Aqua; button4.Text = "Butoane apasate"; }
+        private void button4_MouseLeave(object sender, System.EventArgs e)
+        {
+            textBox1.Visible = false; button4.Text = "Starea butoanelor";
+            button4.BackColor = Color.Orange;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = true; textBox1.Text = "";
+            if (
+                button1.BackColor == Color.Yellow) textBox1.Text = textBox1.Text + 'A';
+            if (
+                button2.BackColor == Color.Yellow) textBox1.Text = textBox1.Text + 'B';
+            if (
+                button3.BackColor == Color.Yellow) textBox1.Text = textBox1.Text + 'C';
+        }
+    }
+}
